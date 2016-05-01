e69d3498f323dd38b10d03942815e4be44814dce
danwheeler <da-wheeler@hotmail.co.uk>
2016-05-01 22:59:54 +0100

    missed last question of chapter 4 complete

--- chapterAnswers/chapter4.js
+++ chapterAnswers/chapter4.js
@@ -62,6 +62,30 @@ var nth = function(list, index) {
 	} 
 };
 
+var deepEqual = function(object1, object2) {
+  if (object1 === object2) return true;
+  
+  if (object1 == null || typeof object1 != "object" ||
+      object2 == null || typeof object2 != "object")
+    return false;
+  
+  var propsInObject1 = 0, 
+  		propsInObject2 = 0;
+
+  for (var prop in object1) {
+    propsInObject1 += 1;
+  }
+
+  for (var prop in object2) {
+    propsInObject2 += 1;
+    if (!(prop in object1) || !deepEqual(object1[prop], object2[prop])) {
+      return false;
+    }
+  }
+
+  return propsInObject1 == propsInObject2;
+};
+
 
 
 
--- chapterTests/chapter4Spec.js
+++ chapterTests/chapter4Spec.js
@@ -1,4 +1,4 @@
-describe('print range between two numbers with third argument as step', function() {
+describe('range - print range between two numbers with third argument as step', function() {
 	it('question 1', function() {
 		var result = range(1,10,1);
 		expect(result).toEqual([1,2,3,4,5,6,7,8,9,10]);
@@ -9,8 +9,8 @@ describe('print range between two numbers with third argument as step', function
 		expect(result).toEqual([5,4,3,2]);
 	});
 
-	describe('sums an array of numbers', function() {
-		it('question 3', function() {
+	describe('sum', function() {
+		it('sums an array of numbers', function() {
 			var result = sum([1,2,3,4,5,6,7,8,9,10]);
 			expect(result).toEqual(55);
 		});
@@ -20,42 +20,60 @@ describe('print range between two numbers with third argument as step', function
 describe('lists', function() {
 
 	describe('inverts the order of an array', function() {
-		it('by creating a new array', function() {
+		it('reverseArray - by creating a new array', function() {
 			var result = reverseArray(['A','B','C','D']);
 			expect(result).toEqual(['D','C','B','A']);
 		});
-		it('modifies existing array', function() {
+		it('reverseArrayInPlace - modifies existing array', function() {
 			var arrayValue = [1,2,3,4,5];
 			reverseArrayInPlace(arrayValue);
 			expect(arrayValue).toEqual([5,4,3,2,1]);
 		});
 	});
 
-	describe('converts an array to a list', function() {
-		it('question 1', function() {
+	describe('arrayToList', function() {
+		it('converts an array to a list', function() {
 			var result = arrayToList([10,20]);
 			expect(result).toEqual({value: 10, rest: {value: 20, rest: null}})
 		});
 	});
 
-	describe('converts a list to an array', function() {
-		it('question 2', function() {
+	describe('listToArray', function() {
+		it('converts a list to an array', function() {
 			var result = listToArray({value: 10, rest: {value: 20, rest: {value: 30, rest: null}}})
 			expect(result).toEqual([10,20,30]);
 		});
 	});
 
-	describe('adds an element to the front of a list', function() {
-		it('question 3', function() {
+	describe('prepend', function() {
+		it('adds an element to the front of a list', function() {
 			var result = prepend(10, prepend(20, null));
 			expect(result).toEqual({value: 10, rest: {value: 20, rest: null}});
 		});
 	});
 
-	describe('takes a list and a number and returns the element at the given position in the list', function() {
-		it('question 4', function() {
+	describe('nth', function() {
+		it('takes a list and a number and returns the element at the given position in the list', function() {
 			var result = nth({value: 10, rest: {value: 20, rest: {value: 30, rest: null}}}, 1);
 			expect(result).toEqual(20);
+		});
+	});
+
+	describe('deepEqual - it returns true only if two values are the same value or are objects with the same properties whose values are also equal', function() {
+		it('question 1', function() {
+			var obj = {here: {is: "an"}, object: 2};
+			var result = deepEqual(obj, obj);
+			expect(result).toEqual(true);
+		});
+		it('question 2', function() {
+			var obj = {here: {is: "an"}, object: 2};
+			var result = deepEqual(obj, {here: 1, object: 2});
+			expect(result).toEqual(false);
+		});
+		it('question 3', function() {
+			var obj = {here: {is: "an"}, object: 2};
+			var result = deepEqual(obj, {here: {is: "an"}, object: 2});
+			expect(result).toEqual(true);
 		});
 	});
 
